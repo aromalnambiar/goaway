50dfbf412e5cd4f6253931a74be581efcd08d6cc
aromalnambiar <aromalndr@gmail.com>
2024-09-05 21:40:54 +0530

    node js connect mongodb database

---
+++ server.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,  
+}).then(con => {
+  console.log(con.connection);
+  console.log("DB Connection successfully established");
+}).catch(err => {
+  console.error('MongoDB connection error:', err);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
